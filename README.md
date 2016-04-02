7d3d7f0ea94d9eaac4f2a5fd6cfa9c5aa5885418
Boldizsár Alföldi <boldizsar.alfoldi@gmail.com>
2016-04-02 23:25:48 +0200

    Test readme 9

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ I explored the `Longstaff - Schwartz two-factor general equilibrium term structu
 This GitHub page is a collection of what I accomplished, found. I wanted to make this public in order to help fellow researchers.
 The project is implemented in `R`.
 
+In my thesis I present two calibration technique:
+1) Long-run calibration
+2) Market calibrtion
+
 **Contents:**
 - [x] Database about the Hungarian Government bonds from 2003 to 2015 
 - [x] Scripts for cleaning and correcting the raw data
@@ -20,13 +24,25 @@ The project is implemented in `R`.
 - [x] Script for complete loop over the trading days between 2003 and 2015 to calculate the daily zero-coupon yield curve 
 
 ---
-#### Long run calibration of the Longstaff-Schwartz model
+#### Long-run calibration of the Longstaff-Schwartz model
 ---
+
+The long-run calibration technique I present can be used for multiple purposes. 
+One purpose is to calibrate the model in-line with long-run economical trends and movements. 
+The other valid usage is to estimate parameters for a market / short-run calibration.
 
 ![alt text](https://github.com/alfoldib/longstaff-schwartz_model/blob/master/plots/plotGMMfit.png "Plot about the GMM fit")
 
 ---
 #### Calibration of the Longstaff-Schwartz model to observed market prices
 ---
+
+<iframe src="https://plot.ly/~Alfoldib/155/hungarian-government-bond-zero-coupon-yield-curve/.embed"
+        height="600" width="100%"
+        scrolling="no" seamless="seamless"
+        frameBorder="0">
+</iframe>
+
+<img src="https://plot.ly/~Alfoldib/155/hungarian-government-bond-zero-coupon-yield-curve/.png">
 
 ![alt text](https://github.com/alfoldib/longstaff-schwartz_model/blob/master/plots/hunGov_zero-coupon_yield.png "Plot hungarian government bond zero curve")
--- scripts/loop over trading days/DE Optimization - market calibration - estimate daily yield curve with starting values.R
+++ scripts/loop over trading days/DE Optimization - market calibration - estimate daily yield curve with starting values.R
@@ -1,19 +1,27 @@
 # Calibrating the model to market prices and estimating the zero coupon yield curve
-
+# With the addition of starting values aka initial population
+# This helps the convergence of the optimization procedure
 rm(list=ls(all=TRUE))
 gc()
 
 require(DEoptim)
 require(parallel)
 require(data.table)
+require(foreach)
 
 setwd("C:/Users/Boldi/Documents/Publish/Interest rate models/longstaff-schwartz_model")
 
+
 # Loading data and functions
-source("./scripts/load data.R")
+source("./scripts/support scripts/load data.R")
 source("./functions/longstaff-schwarcz functions.R")
 source("./functions/goodness-of-fit functions.R")
 source("./functions/support functions for DE optimization.R")
+
+# Load start parameters 
+# - these are calculated with the GMM calibrating procedure
+# - for detailed info see "get starting values for optimization - GMM.R" script
+load("./Data/start parameters.rdata")
 
 # Getting currency for each bond id (alias)
 ccy <- cf_data[, .N, by = list(alias, currency)]
@@ -30,9 +38,9 @@ prep_cf_data <- cf_data[interest_type != "FLOAT" & currency == "HUF",
                           cf_amount = ((ifelse(is.na(principal_amount) | principal_amount < 0, 
                                                0, 
                                                principal_amount) + 
-                                         ifelse(is.na(interest_amount), 
-                                                0, 
-                                                interest_amount)) / 
+                                          ifelse(is.na(interest_amount), 
+                                                 0, 
+                                                 interest_amount)) / 
                                          face_value * 100)
                         )]
 prep_cf_data <- prep_cf_data[cf_amount != 0]
@@ -40,14 +48,14 @@ prep_cf_data <- prep_cf_data[cf_amount != 0]
 # Preparation of market prices 
 # - filtering out those prices where I don't have cash-flow
 prep_prices <- best_price[,
-                           list(
-                             alias, 
-                             date, 
-                             bid = bid_net_price,
-                             ask = ask_net_price,
-                             acc_int = accrued_interest,
-                             price = gross_mid_price
-                            )]
+                          list(
+                            alias, 
+                            date, 
+                            bid = bid_net_price,
+                            ask = ask_net_price,
+                            acc_int = accrued_interest,
+                            price = gross_mid_price
+                          )]
 
 prep_prices <- merge(prep_prices, prep_cf_data[, .N, by = list(alias, issue_date, maturity_date)], by = "alias")
 prep_prices[, N := NULL]
@@ -59,10 +67,13 @@ param_name <- c("alpha", "beta", "gamma", "delta", "eta", "nu")
 dates <- sort(unique(best_price$date))
 dates <- dates[dates >= as.Date("2015-06-01") & dates < as.Date("2015-07-01")]
 
+# Optional place to calibrate to a single trading day
+# if you choose to run the program without the loop
+curr_date <- as.Date("2004-04-09")
+
 # Looping through all dates from the dataset
 # !! It takes a while, first run it for 1 date!!
 for (curr_date in dates) {
-  
   # If you run it without the loop, please add a relevant date here
   curr_date <- as.Date(curr_date, origin = "1970-01-01")
   
@@ -81,7 +92,9 @@ for (curr_date in dates) {
   curr_bonds <- merge(prep_cf_data, curr_prices[, list(alias)], by = "alias")
   
   # Creating the cash-flow matrix
-  cf_matrix <- dcast(curr_bonds[cf_date > curr_date], cf_date ~ alias, value.var = "cf_amount", fill = 0)
+  cf_matrix <- dcast.data.table(curr_bonds[cf_date > curr_date], 
+                                cf_date ~ alias, 
+                                value.var = "cf_amount", fill = 0)
   cf_matrix[, t := (cf_date - curr_date) / 365]
   
   # Transform cash-flow data.table into matrix variable type
@@ -93,16 +106,57 @@ for (curr_date in dates) {
   curr_yield <- state_data[date == curr_date & short_name == "yield", value]
   curr_var   <- state_data[date == curr_date & short_name == "var", value]
   
-  # Determine lower and upper bound of parameter search space
+  # Getting start parameters from database
+  # - Calculated with GMM 
+  # (see script "long run calibration - starting values for DE optimization - GMM")
+  curr_start_params <- start_params[target_date == curr_date]
+  
+  # Determine bounds for state variables
   var_yield_rate <- curr_var / curr_yield
-  lower_bound    <- c(0, var_yield_rate, 0, 0, 0, -100)
-  upper_bound    <- c(var_yield_rate, 100, 100, 100, 100, 100)
+  
+  # Determine lower and upper bound of parameter search space
+  if (curr_start_params[var_name == "alpha", coeff] > curr_start_params[var_name == "beta", coeff]) {
+    lower_bound    <- c(var_yield_rate, 0, 0, 0, 0, -10000)
+    upper_bound    <- c(10000, var_yield_rate, 10000, 10000, 10000, 10000)
+    
+  } else {
+    lower_bound    <- c(0, var_yield_rate, 0, 0, 0, -10000)
+    upper_bound    <- c(var_yield_rate, 10000, 10000, 10000, 10000, 10000)
+    
+  }
+  
+  # Determine the number of parameters
+  n <- length(lower_bound) 
+  
+  # Determine the size of the population 
+  # (it should by at least 10 times bigger than the number of parameters)
+  npop <- n * 20
+  
+  # Replicating the previously extracted starting parameters
+  init_pop <- matrix(rep(curr_start_params[,coeff], npop), ncol = n, byrow = T)
+
+  # Generating noise with expected value of zero and low variance
+  # - for DE optim we have to add noise to the initial population
+  noise <- matrix((runif(n * npop) - .5) / 100, ncol = n)
+  
+  # - if the starting values are close to zero these can be negative
+  #   which is not interpretable given the lower bounds of parameters
+  init_pop <- init_pop + noise
+  
+  # -> if the starting values with noise goes negative, 
+  #    then replacing them with zero, in order not to breach lower bounds
+  lb_mat <- matrix(rep(lower_bound, npop), ncol = n, byrow = T)
+  init_pop[init_pop < lb_mat] <- 0
+  
+  # Optional - Check expected values before and after adding noise
+  #            Only effective if it's not run in loop
+  # rbind(apply(init_pop, 2, mean), curr_start_params[,coeff])
   
   # Searching the optimal parameter vector with DE optimization
   # - Parallel computing, with all available cores you have on the computer
   optimum <- DEoptim(obj_function, lower_bound, upper_bound, 
-                     control = list(trace = F, itermax = 5000, 
-                                    parallelType = 1, 
+                     control = list(trace = 100, itermax = 5000, NP = npop, initialpop = init_pop, 
+                                    parallelType = 2, 
                                     parVar = c("curr_prices", "curr_cf_matrix",
                                                "curr_yield", "curr_var",
                                                "wmae", "mod_discount",
